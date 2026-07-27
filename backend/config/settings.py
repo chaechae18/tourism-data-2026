b3223595e8bba92c6359abde1c802ab869a55b5d
Chyoung812 <kcy960812@gmail.com>
2026-07-27 20:06:29 +0900

    chore(backend): allow all localhost CORS origins in DEBUG mode

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,7 +78,12 @@ REST_FRAMEWORK = {
     "UNAUTHENTICATED_USER": None,
 }
 
-CORS_ALLOWED_ORIGINS = env_list("CORS_ALLOWED_ORIGINS", "http://localhost:3000")
+CORS_ALLOWED_ORIGINS = env_list(
+    "CORS_ALLOWED_ORIGINS",
+    "http://localhost:3000,http://localhost:3001,http://localhost:3002,http://127.0.0.1:3000,http://127.0.0.1:3002",
+)
+CORS_ALLOW_ALL_ORIGINS = DEBUG
+
 
 LANGUAGE_CODE = "ko"
 TIME_ZONE = "Asia/Seoul"
